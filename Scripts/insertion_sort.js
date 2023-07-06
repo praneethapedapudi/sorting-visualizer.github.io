41b63670046332a57f7ebc252150c7d1184a5003
praneethapedapudi <praneetha@kgpian.iitkgp.ac.in>
2023-07-07 01:15:29 +0530

    sorting visualizer
    additional feature: dark mode

---
+++ Scripts/insertion_sort.js
@@ -0,0 +1,50 @@
+function Insertion()
+{
+    //Setting Time complexities
+    document.getElementById("time_worst").innerText="O(N^2)";
+    document.getElementById("time_avg").innerText="Θ(N^2)";
+    document.getElementById("time_best").innerText="Ω(N)";    
+
+    //Setting Space complexity
+    document.getElementById("space_worst").innerText="O(1)";
+
+    c_delay=0;
+
+    for(var j=0;j<array_size;j++)
+    {
+        div_update(divs[j],div_sizes[j],"yellow");//Color update
+
+        var key= div_sizes[j];
+        var i=j-1;
+        while(i>=0 && div_sizes[i]>key)
+        {
+            div_update(divs[i],div_sizes[i],"red");//Color update
+            div_update(divs[i+1],div_sizes[i+1],"red");//Color update
+
+            div_sizes[i+1]=div_sizes[i];
+
+            div_update(divs[i],div_sizes[i],"red");//Height update
+            div_update(divs[i+1],div_sizes[i+1],"red");//Height update
+    
+            div_update(divs[i],div_sizes[i],"blue");//Color update
+            if(i==(j-1))
+            {
+                div_update(divs[i+1],div_sizes[i+1],"yellow");//Color update
+            }
+            else
+            {
+                div_update(divs[i+1],div_sizes[i+1],"blue");//Color update
+            }
+            i-=1;
+        }
+        div_sizes[i+1]=key;
+
+        for(var t=0;t<j;t++)
+        {
+            div_update(divs[t],div_sizes[t],"green");//Color update
+        }
+    }
+    div_update(divs[j-1],div_sizes[j-1],"green");//Color update
+
+    enable_buttons();
+}
