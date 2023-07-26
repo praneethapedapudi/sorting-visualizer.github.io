73bd4716dc4430ec5bd0053c2491cbcbd46f29bf
praneethapedapudi <99593993+praneethapedapudi@users.noreply.github.com>
2023-07-26 13:23:42 +0530

    Update quick_sort.js

--- Scripts/quick_sort.js
+++ Scripts/quick_sort.js
@@ -1,12 +1,12 @@
 function Quick()
 {
     //Setting Time complexities
-    document.getElementById("time_worst").innerText="O(N^2)";
-    document.getElementById("time_avg").innerText="Θ(N log N)";
+    document.getElementById("time_worst").innerText="O(n^2)";
+    document.getElementById("time_avg").innerText="Θ(n log n)";
     document.getElementById("time_best").innerText="Ω(N log N)";
 
     //Setting Space complexity
-    document.getElementById("space_worst").innerText="O(log N)";
+    document.getElementById("space_worst").innerText="O(log n)";
 
     c_delay=0;
 
